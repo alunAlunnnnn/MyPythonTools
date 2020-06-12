ec9ecfdbec2159f406bd2f59255a659b494d21e5
alunAlunnnnn <517308447@qq.com>
2020-06-12 09:46:39 +0800

    pull from git hub

--- CommonTool/PictureProcess/读写二进制图片.py
+++ CommonTool/PictureProcess/读写二进制图片.py
@@ -37,6 +37,20 @@ class BinPictureReader:
             raise NotExist
 
 
-binPic = r'D:\a\nodes_0_textures_0.txt'
+    def saveToJPG(self, outputPath, outputName):
+        if os.path.exists(outputPath):
+            if os.path.isdir(outputPath):
+                with open(os.path.join(outputPath, str(outputName) + '.bin.dds.gz'), 'wb') as f:
+                    f.write(self.binData)
+            else:
+                print('the directory of outputPath is not available')
+                raise NotDir
+        else:
+            print('the directory of outputPath is not exist')
+            raise NotExist
+
+
+# binPic = r'D:\a\nodes_0_textures_0.txt'
+binPic = r'D:\a\nodes_0_textures_0_0_1.txt'
 binReader = BinPictureReader(binPic)
-binReader.saveToPNG('D:/a', 'my_reader')
+binReader.saveToJPG('D:/a', 'my_reader2')
